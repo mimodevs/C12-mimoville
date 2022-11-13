1fe195fc70d50f306d4cda1e1e6eab58a1dc5cba
RoloAr <rolondoarias@gmail.com>
2022-11-12 19:47:54 -0600

    changes for the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,48 +9,46 @@ const Footer = () => {
 
       <section>
         <a
-          href="#!"
-          role="button"
-          ><i class="fab fa-facebook-f">facebook</i
-        ></a>
+          href=""
+          role="button">
+          <img src="facebookicon.png"></img> 
+          </a>
   
         <a
           href="#!"
-          role="button"
-          ><i class="fab fa-twitter"></i
-        ></a>
+          role="button">
+          <img src="facebookicon.png"></img> 
+          </a>
   
         <a
           href="#!"
-          role="button"
-          ><i class="fab fa-google"></i
-        ></a>
+          role="button">
+             <img src="facebookicon.png"></img> 
+          </a>
   
         <a
           href="#!"
-          role="button"
-          ><i class="fab fa-instagram"></i
-        ></a>
+          role="button">
+           <img src="facebookicon.png"></img> 
+        </a>
   
         <a
           href="#!"
-          role="button"
-          ><i class="fab fa-linkedin-in"></i
-        ></a>
+          role="button">
+         <img src="facebookicon.png"></img> 
+        </a>
 
         <a
-
           href="#!"
-          role="button"
-          ><i class="fab fa-github"></i
-        ></a>
+          role="button">
+          <img src="facebookicon.png"></img> 
+          </a>
       </section>
 
     </div>
 
     <div class="text-center p-3" >
       © 2020 Copyright:
-      <a class="text-white" >MDBootstrap.com</a>
     </div>
   </footer>
   );

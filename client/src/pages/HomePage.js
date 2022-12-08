ac84d0075d3cb9368007c1d0cbc90a2a0f56bdc0
twistedstack <kelly@getmimo.com>
2022-12-08 08:38:00 -0500

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,19 +2,15 @@ import React from "react";
 import Records from "../components/records";
 
 const HomePage = () => {
-  return(
+  return (
     <div>
-      
-       <div className="hero">
+      <div className="hero">
         <h2>All about Mimoville! </h2>
         <p>See why we’re the best stop for your next weekend getaway</p>
-       </div>
-       <Records />
+      </div>
+      <Records />
     </div>
-    
   );
-  
-  
 };
 
 export default HomePage;
